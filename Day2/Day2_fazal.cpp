a72a452153cf89d36d780562ec3e1912ca01b243
fazal-10 <fazalmansuri08@gmail.com>
2022-09-03 10:38:31 -0700

    commit

--- Day2/Day2_fazal.cpp
+++ Day2/Day2_fazal.cpp
@@ -12,8 +12,13 @@ int main()
     int a[] = {1, 2, 3, 4, 5, 6, 7};
     int n = sizeof(a) / sizeof(int);
     int k;
+    cout<<"Given array :"<<endl;
+    for(int i=0;i<n;i++)
+    cout<<a[i]<<" ";
+    cout<<endl;
     cout << "Enter number of steps" << endl;
     cin >> k;
+    cout<<"Array after "<<k<<" right rotation :"<<endl;
     right_rotate(a, n, k);
     return 0;
 }
