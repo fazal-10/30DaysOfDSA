7b41e1f3f239e6c66dc30502955163a9b857258c
fazal-10 <fazalmansuri08@gmail.com>
2022-09-03 10:37:40 -0700

    commit day_3

---
+++ Day3/Day3_fazal.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string ans="false";
+    int a[][3]={
+                    {1,2,3},
+                    {4,5,6},
+                    {7,8,9}
+                };
+    int key;
+    cout<<"Enter key"<<endl;
+    cin>>key;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(a[i][j]==key)
+            {
+                ans="true";
+                break;
+            }
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
